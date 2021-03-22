a82be6e4cf932213ba2fdb581db5ad9ffc9884e0
ayumitanaka <tanakayumi13@gmail.com>
2021-03-22 16:35:18 +0000

    assignment

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,30 +1,28 @@
-import React, { useRef } from 'react'
-import { connect } from 'react-redux'
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
 
-const AddForm = ({ newToDo }) => {
-  const refContainer = useRef(null)
+const AddForm = () => {
+  const dispatch = useDispatch();
+
+  const newToDo = (content) => {
+    dispatch({ type: "NEW_TO_DO", payload: content });
+  };
+
+  const refContainer = useRef(null);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    newToDo({ content: refContainer.current.value })
-    refContainer.current.value = ''
-  }
+    e.preventDefault();
+    newToDo({ content: refContainer.current.value });
+    refContainer.current.value = "";
+  };
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>Add new todo:</label>
-        <input type='text' ref={refContainer} />
+        <input type="text" ref={refContainer} />
       </form>
     </div>
-  )
-}
+  );
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    newToDo: (content) => {
-      dispatch({ type: 'NEW_TO_DO', payload: content })
-    },
-  }
-}
-
-export default connect(null, mapDispatchToProps)(AddForm)
+export default AddForm;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,48 +1,53 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import ToDos from './ToDos'
-import AddForm from './AddForm'
-import FilterToDos from './FilterToDos'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ToDos from "./ToDos";
+import AddForm from "./AddForm";
+import FilterToDos from "./FilterToDos";
 
 const ToDoList = (props) => {
-  const { toDoList, removeToDo, markToDo } = props
+  const toDoList = useSelector((state) => state.toDoList);
+  const dispatch = useDispatch();
 
-  const [filterVal, setFilterVal] = useState('SHOW_ALL')
+  const [filterVal, setFilterVal] = useState("SHOW_ALL");
 
-  const deleteToDo = (id) => removeToDo(id)
-  const doneToDo = (id) => markToDo(id)
+  const deleteToDo = (id) => {
+    dispatch({ type: "DELETE_TO_DO", payload: id });
+  };
+  const doneToDo = (id) => {
+    dispatch({ type: "DONE_TO_DO", payload: id });
+  };
 
   const getVisibleToDos = (toDoList, filterVal) => {
     switch (filterVal) {
-      case 'SHOW_COMPLETED':
-        return toDoList.filter((item) => item.isDone)
-      case 'SHOW_ACTIVE':
-        return toDoList.filter((item) => !item.isDone)
-      case 'SHOW_ALL':
-        return toDoList
+      case "SHOW_COMPLETED":
+        return toDoList.filter((item) => item.isDone);
+      case "SHOW_ACTIVE":
+        return toDoList.filter((item) => !item.isDone);
+      case "SHOW_ALL":
+        return toDoList;
       default:
-        break
+        break;
     }
-  }
+  };
 
-  const visibleList = getVisibleToDos(toDoList, filterVal)
+  const visibleList = getVisibleToDos(toDoList, filterVal);
 
   const getCurrentView = (filterVal) => {
     switch (filterVal) {
-      case 'SHOW_COMPLETED':
-        return 'No completed task. Start doing something yo!!'
-      case 'SHOW_ACTIVE':
-        return "No more Todo's!! Yatta!!!"
+      case "SHOW_COMPLETED":
+        return "No completed task. Start doing something yo!!";
+      case "SHOW_ACTIVE":
+        return "No more Todo's!! Yatta!!!";
       default:
-        return 'Start doing something yo!!'
+        return "Start doing something yo!!";
     }
-  }
+  };
 
-  const displayMessage = getCurrentView(filterVal)
+  const displayMessage = getCurrentView(filterVal);
 
   const filterFunc = (action) => {
-    setFilterVal(action)
-  }
+    setFilterVal(action);
+  };
 
   return (
     <>
@@ -56,24 +61,24 @@ const ToDoList = (props) => {
       />
       <AddForm />
     </>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => {
-  return {
-    toDoList: state.toDoList,
-  }
-}
+// const mapStateToProps = (state) => {
+//   return {
+//     toDoList: state.toDoList,
+//   }
+// }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    removeToDo: (id) => {
-      dispatch({ type: 'DELETE_TO_DO', payload: id })
-    },
-    markToDo: (id) => {
-      dispatch({ type: 'DONE_TO_DO', payload: id })
-    },
-  }
-}
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     removeToDo: (id) => {
+//       dispatch({ type: 'DELETE_TO_DO', payload: id })
+//     },
+//     markToDo: (id) => {
+//       dispatch({ type: 'DONE_TO_DO', payload: id })
+//     },
+//   }
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ToDoList)
+export default ToDoList;
